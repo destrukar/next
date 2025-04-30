da76f13fd8c3cf2cd42e093ea7e1e03fc8bbf11d
nicholas <nicholasdes9d6@gmail.com>
2025-04-30 17:30:05 -0300

    funcional 4

--- src/app/api/influencers/[id]/avaliar/route.ts
+++ src/app/api/influencers/[id]/avaliar/route.ts
@@ -1,10 +1,14 @@
-import { Request, NextResponse } from 'next/server';
-import { prisma } from '@/src/app/lib/prisma';
 
-// Ajustando a assinatura da função para compatibilidade com o Next.js 13+
-export async function PUT(request: Request, { params }: { params: { id: string } }) {
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import type { NextApiRequest } from 'next';
+
+interface RouteContext {
+  params: { id: string };
+}
+export async function PUT(request: Request, context: RouteContext) {
   try {
-    const { id } = params;  // Aqui pegamos o ID diretamente dos parâmetros
+    const { id } = context.params;  // Aqui pegamos o ID diretamente dos parâmetros
 
     // Lê os dados da requisição
     const data = await request.json();
