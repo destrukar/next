b2dd3ca89a69246de69e1f37e3dab1a363e712a2
nicholas <nicholasdes9d6@gmail.com>
2025-04-30 19:01:04 -0300

    funcional(?)21

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -46,15 +46,18 @@ const authOptions = {
     async signIn({ user }: { user: User }) {
       try {
         if (!user?.email) return false;
-
+    
         let existingUser = await prisma.usuario.findUnique({
           where: { email: user.email },
         });
-
+    
+        // Verifica se o nome do usuário não é nulo ou indefinido e usa um valor padrão caso seja
+        const userName = user.name || "Nome padrão"; // Use um nome padrão ou algo relevante
+    
         if (!existingUser) {
           await prisma.usuario.create({
             data: {
-              nome: user.name,
+              nome: userName,  // Passa o nome verificado
               email: user.email,
             },
           });
@@ -64,7 +67,7 @@ const authOptions = {
         console.error("Erro no signIn:", error);
         return false;
       }
-    },
+    }
     async jwt({ token, user }: { token: JWT; user?: User }) {
       if (user) {
         token.user = user;
