84d200b7002cecb529701c1b6ba6ca8f8ca28069
nicholas <nicholasdes9d6@gmail.com>
2025-04-30 17:32:56 -0300

    funcional 5

--- src/app/api/influencers/[id]/avaliar/route.ts
+++ src/app/api/influencers/[id]/avaliar/route.ts
@@ -1,28 +1,22 @@
-
 import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import type { NextApiRequest } from 'next';
+import { prisma } from '@/src/app/lib/prisma';
 
-interface RouteContext {
-  params: { id: string };
-}
-export async function PUT(request: Request, context: RouteContext) {
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    const { id } = context.params;  // Aqui pegamos o ID diretamente dos parâmetros
-
-    // Lê os dados da requisição
+    const id = parseInt(params.id);
     const data = await request.json();
 
-    // Atualiza o influencer no banco de dados usando o ID da rota
     const updatedInfluencer = await prisma.influencer.update({
-      where: { id: parseInt(id) },  // Garantindo que o ID seja inteiro
-      data,  // Atualizando com os dados recebidos
+      where: { id },
+      data,
     });
 
-    // Retorna a resposta com o influencer atualizado
-    return Response.json(updatedInfluencer, { status: 200 });
+    return NextResponse.json(updatedInfluencer, { status: 200 });
   } catch (error) {
     console.error('Erro ao atualizar o influencer', error);
-    return Response.json({ error: 'Erro ao atualizar influencer' }, { status: 500 });
+    return NextResponse.json({ error: 'Erro ao atualizar influencer' }, { status: 500 });
   }
 }
